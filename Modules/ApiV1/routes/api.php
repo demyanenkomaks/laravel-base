79a0ac310bbfddebbc85fbf19abeba37d9a8000a
demyanenkomaks <demyanenkomaks0@gmail.ru>
2025-05-21 15:46:46 +0300

    upd: include modules for check rector and phpstan

--- Modules/ApiV1/routes/api.php
+++ Modules/ApiV1/routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('', fn () => [
+Route::get('', fn (): array => [
     'message' => 'API check works!',
     'version' => '1.0.0',
 ]);
--- Modules/ApiV1/tests/Feature/CheckingRequestsTest.php
+++ Modules/ApiV1/tests/Feature/CheckingRequestsTest.php
@@ -1,7 +1,7 @@
 <?php
 
-test('запрос /api/v1 выполнился успешно', function () {
-    $response = $this->get('/api/v1');
+test('запрос /api/v1 выполнился успешно', function (): void {
+    $response = $this->get('/api/v1'); // @phpstan-ignore method.notFound
 
     $response->assertStatus(200);
 });
